97ba12a005eb832820af3f608687cbdc0496f97d
max-l <236820310@qq.com>
2017-01-05 00:44:46 +0800

    output相关的注释

--- 20170102webpackReactES6Redux/webpack.config.js
+++ 20170102webpackReactES6Redux/webpack.config.js
@@ -20,6 +20,52 @@ module.exports = {
     3. entry的值是对象，可以构建多个bundle
   */
   entry: './js/entry.js',    //这个入口是非热部署的
+  //***入口的额外介绍  ***
+  //entry可以是字符串（单入口），可以是数组（多入口），但为了后续发展，请务必使用object，因为object中的key在webpack里相当于此入口的name，既可以后续用来拼生成文件的路径，也可以用来作为此入口的唯一标识。我推荐的形式是这样的：
+  /*
+  entry: { // pagesDir是前面准备好的入口文件集合目录的路径 
+    'alert/index': path.resolve(pagesDir, `./alert/index/page`),  
+    'index/login': path.resolve(pagesDir, `./index/login/page`),  
+    'index/index': path.resolve(pagesDir, `./index/index/page`), 
+  },
+  */
+  //***我们项目热部署的入口  ***
+  /*entry:{
+    'index': [
+      'webpack-hot-middleware/client?path=http://localhost:' + siteConfig.port + '/__webpack_hmr',
+      './js/index'
+    ],
+    'setting': [
+      'webpack-hot-middleware/client?path=http://localhost:' + siteConfig.port + '/__webpack_hmr',
+      './js/index_setting'
+    ]
+  }*/
+  //*** 生产环境的入口  ***
+  /*config.entry = {
+    // webpack 会自动打包公共的，这两个是兼容文件。入口不会引入，股灾commons 做文章
+    'commons': [
+      'core-js/es6/object.js',
+      'core-js/es6/promise.js',
+      'react-fastclick',
+      'react',
+      'react-dom',
+      'react-router',
+      'redux',
+      'react-redux',
+      'redux-async-actions-reducers',
+      'redux-thunk',
+      'classnames',
+      'js-cookie',
+      './antd/dist/antd.min.js'
+    ],
+    'index': [
+      './js/index'
+    ],
+    'setting': [
+      './js/index_setting'
+    ]
+  };*/
+  
   /*
     输出的路径+文件
   */
@@ -27,6 +73,36 @@ module.exports = {
     path: 'pack', //文件夹名
     filename: 'bundle.js'
   }
+
+  //path 和 filename 针对入口文件 publicPath针对css/img/is/等文件路径（相对于浏览器）chunkFilename是其他模块打包后的文件名
+  //http://mp.weixin.qq.com/s?__biz=MzA5NTM2MTEzNw==&mid=2736710855&idx=2&sn=b08f6235f506c838efb01ec2ec3c793f&chksm=b6aac5d981dd4ccf2b903b711680aa31125936cf1dff27b650c2c0be7b1a0f86beb468f1f960&scene=21#wechat_redirect
+  /*output: { 
+    
+    path: buildDir, // var buildDir = path.resolve(__dirname, './build'); 
+    //path参数表示生成文件的根目录，需要传入一个绝对路径。path参数和后面的filename参数共同组成入口文件的完整路径。
+
+    publicPath: '../../../../build/', 
+    //publicPath参数表示的是一个URL路径（指向生成文件的根目录），用于生成css/js/图片/字体文件等资源的路径，以确保网页能正确地加载到这些资源。
+    //publicPath参数跟path参数的区别是：path参数其实是针对本地文件系统的，而publicPath则针对的是浏览器；
+    //因此，publicPath既可以是一个相对路径，如示例中的'../../../../build/'，也可以是一个绝对路径如http://www.xxxxx.com/。一般来说，我还是更推荐相对路径的写法，这样的话整体迁移起来非常方便。那什么时候用绝对路径呢？其实也很简单，当你的html文件跟其它资源放在不同的域名下的时候，就应该用绝对路径了，这种情况非常多见于后端渲染模板的场景。
+
+    filename: '[name]/entry.js',    // [name]表示entry每一项中的key，用以批量指定生成后文件的名称 
+      
+      //[name]，指代入口文件的name，也就是上面提到的entry参数的key，因此，我们可以在name里利用/，即可达到控制文件目录结构的效果。
+      //[hash]，指代本次编译的一个hash版本，值得注意的是，只要是在同一次编译过程中生成的文件，这个[hash]的值就是一样的；在缓存的层面来说，相当于一次全量的替换。
+      //[chunkhash]，指代的是当前chunk的一个hash版本，也就是说，在同一次编译中，每一个chunk的hash都是不一样的；而在两次编译中，如果某个chunk根本没有发生变化，那么该chunk的hash也就不会发生变化。这在缓存的层面上来说，就是把缓存的粒度精细到具体某个chunk，只要chunk不变，该chunk的浏览器缓存就可以继续使用。
+      
+
+    chunkFilename: '[id].bundle.js', 
+    //chunkFilename参数与filename参数类似，都是用来定义生成文件的命名方式的，只不过，chunkFilename参数指定的是除入口文件外的chunk（这些chunk通常是由于webpack对代码的优化所形成的，比如因应实际运行的情况来异步加载）的命名。
+  },*/
+  /*我们项目中的*/
+  /*output: {
+    path: path.join(__dirname, env === 'development' ? "build" : "dist"),
+    filename: env === 'development' ? 'js/[name].[hash].bundle.js' : 'js/[name].[chunkhash].bundle.js',
+    chunkFilename: env === 'development' ? 'js/[id].[chunkhash].bundle.js' : 'js/[id].[chunkhash].bundle.js',
+    publicPath: siteConfig.publicPath
+  },*/
   
   ,module: {
     //loaders:可以根据模块类型（扩展名、类型名）来自动绑定需要的 loader。
