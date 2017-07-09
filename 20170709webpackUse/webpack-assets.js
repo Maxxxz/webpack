1ebbca87d9aff3e867e4c20b368a5e287bea81ab
张翔茜-h <236820310@qq.com>
2017-07-09 20:03:57 +0800

    测试多个webpack2 中多个commmonchunks

---
+++ 20170709webpackUse/webpack-assets.js
@@ -0,0 +1,1 @@
+window.staticMap = {"moment":{"js":"./dist/moment.03cc38c003c564483c46.bundle.js"},"pageOne":{"js":"./dist/pageOne.c806b567a26693edb969.bundle.js"},"echarts":{"js":"./dist/echarts.320acb17fca6bd186af7.bundle.js"}}
