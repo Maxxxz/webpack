2fc473b5b3e346b0b48639b9505de94c6259ca6b
maxizhang <maxizhang@tencent.com>
2019-03-27 16:00:50 +0800

    feat: 添加自动打开和命令行

--- node-anydoor/src/app.js
+++ node-anydoor/src/app.js
@@ -2,16 +2,27 @@ const http = require('http');
 const path = require('path');
 const chalk = require('chalk');
 const conf = require('./config/defaultConfig');
-
 const route = require('./helper/router');
+const openUrl = require('./helper/openUrl.js');
 
-const server = http.createServer((req, res) => {
-  const filePath = path.join(conf.root, req.url);
-  console.log('filePath', filePath);
-  route(req, res, filePath);
-});
+class Server {
+  constructor(config) {
+    this.conf = Object.assign({}, conf, config);
+  }
 
-server.listen(conf.port, conf.hostname, () => {
-  const addr = `http://${conf.hostname}:${conf.port}`;
-  console.log(`Server listen on ${chalk.green(addr)}`);
-});
+  start() {
+    const server = http.createServer((req, res) => {
+      const filePath = path.join(this.conf.root, req.url);
+      console.log('filePath', filePath);
+      route(req, res, filePath);
+    });
+
+    server.listen(this.conf.port, this.conf.hostname, () => {
+      const addr = `http://${this.conf.hostname}:${this.conf.port}`;
+      console.log(`Server listen on ${chalk.green(addr)}`);
+      openUrl(addr)
+    });
+  }
+}
+
+module.exports = Server;
